62cbcbf9c32721873e3ba9120307305a6bd1cc48
Stephanie Thompson <steph@stephcodes.dev>
2025-05-08 23:32:57 -0500

    First commit with Hero section partially created

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import React from 'react'
+import { Hero } from '@/components'
+
+export default function Home() {
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+    </main>
+  )
+}
