4597404963a27b90149f260279b599cfef265c97
Stephanie Thompson <steph@stephcodes.dev>
2025-05-14 22:00:51 -0500

    Added new images to public folder

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,6 +23,12 @@ const Hero = () => {
               handleClick={handleScroll}
             />
         </div>
+
+        <div className="hero__image-container">
+          <div className="hero__image">
+            <Image src="" />
+          </div>
+        </div>
     </div>
   )
 }
