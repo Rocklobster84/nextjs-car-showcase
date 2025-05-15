cee0db576521c07448494de840b38dfdcdc25edd
Stephanie Thompson <steph@stephcodes.dev>
2025-05-14 22:30:18 -0500

    Replaced missing closing body tag in layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Footer, Navbar } from '@/components'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        </body>
     </html>
   )
 }
