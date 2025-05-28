9f282bbff22a3bdab74f2210a7f95141463c7930
Stephanie Thompson <steph@stephcodes.dev>
2025-05-27 23:01:20 -0500

    Created Car Card component. Updated Custom Button component to include text styles and a right icon option. Created utils to fetch data from the cars API and used it in the main page file. Created a new interface for Car Props.

---
+++ utils/index.ts
@@ -0,0 +1,27 @@
+export async function fetchCars() {
+    const headers = {
+    'x-rapidapi-key': '6343fd0fcdmsh1041c2b62befcdfp185688jsnb11fe019aecf',
+    'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+  }
+
+  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+    headers: headers,
+  });
+
+  const result = await response.json(); 
+
+  return result;
+}
+
+export const calculateCarRent = (year: number) => {
+    const basePricePerDay = 50;
+    const mileageFactor = 0.1;
+    const ageFactor = 0.05;
+
+    const mileageRate = 25 * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+    return rentalRatePerDay.toFixed(0);
+}
