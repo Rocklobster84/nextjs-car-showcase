51b890a225a38b32b7c7674bf5f1de53afc6d700
Stephanie Thompson <steph@stephcodes.dev>
2025-06-03 20:43:25 -0500

    Implemented Car Image API originally used. Will need to replace with something not locked down

--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,12 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
     const headers = {
     'x-rapidapi-key': process.env.API_KEY,
-    'x-rapidapi-host': process.env.API_HOST
+    'x-rapidapi-host': "cars-by-api-ninjas.p.rapidapi.com"
   }
 
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
+  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
     headers: headers,
   });
 
@@ -24,4 +26,18 @@ export const calculateCarRent = (year: number) => {
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
     return rentalRatePerDay.toFixed(0);
+}
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage')
+  const { make, year, model } = car;
+
+  url.searchParams.append('customer', 'hrjavascript-mastery');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
 }
